8bf9ab73e70f5d7affeac028a484ec8d516b4fc2
ciyanogen <ttbpccorporation@outlook.com>
2018-07-23 09:08:44 +0300

    Update sepolicy.mk

--- sepolicy.mk
+++ sepolicy.mk
@@ -6,4 +6,4 @@ BOARD_SEPOLICY_DIRS := \
        device/qcom/sepolicy/test \
        device/qcom/sepolicy/$(TARGET_BOARD_PLATFORM)
 
--include vendor/viper/sepolicy/qcom/sepolicy.mk
+-include vendor/ecem/sepolicy/qcom/sepolicy.mk
